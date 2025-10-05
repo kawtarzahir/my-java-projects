e2cdb3ced1534dd0615d063051d920eed98217d7
kawtarzahir <zkawtar.1401@gmail.com>
2025-10-05 14:59:19 +0200

    Update PredatorPreyDemo.java
    Demonstrating interfaces and OOP

--- PredatorPreyDemo.java
+++ PredatorPreyDemo.java
@@ -20,14 +20,29 @@ public class Lion implements Predator {
     System.out.println("The lion starts the attack!");
   }
 }
+// Class implementing both Prey and Predator
+public class Fish implements Prey, Predator {
+    @Override
+    public void flee() {
+    System.out.println("The fish swims away quickly!");
+    }
+
+    @Override
+    public void hunt() {
+    System.out.println("The fish hunts smaller fish.");
+  }
+}
 //Main class
 public class Main {
   public static void main(String[] args) {
     Lion lion = new Lion();
     Gazelle gazelle = new Gazelle();
+    Fish fish = new Fish();
 
        gazelle.flee();
        lion.hunt();
+       fish.flee();
+       fish.hunt();
   }
 }
     
